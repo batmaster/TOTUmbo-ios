e2d356d17c79c2c7aacdd31ed8703119a3de6207
batmaster <batmaster_kn@hotmail.com>
2015-08-20 16:21:59 +0700

    edit prototype cell bug cannot run emu

--- TOTUmbo/DownViewController.swift
+++ TOTUmbo/DownViewController.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var pickerViewProvinces: UIPickerView!
+    @IBOutlet weak var listView: UITableView!
     
-    var pickerDataSource: NSMutableArray = ["a"];
+    var pickerDataSource: NSMutableArray = ["a"]
+    var listViewDataSource: NSMutableArray = ["b"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pickerViewProvinces.dataSource = self;
-        pickerViewProvinces.delegate = self;
+        
+        pickerViewProvinces.dataSource = self
+        pickerViewProvinces.delegate = self
+        
+        listView.dataSource = self
+        listView.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -25,12 +31,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    
+    
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerDataSource.count;
+        return pickerDataSource.count
     }
     
     func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
@@ -40,6 +50,41 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         pickerLabel.attributedText = myTitle
         return pickerLabel
     }
+    
+    
+    
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listViewDataSource.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TextCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let row = indexPath.row
+        cell.textLabel?.text = listViewDataSource[row] as? String
+        
+        return cell
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        print(listViewDataSource[row])
+    }
+    
+    
+    
+    
+    
+    
+    
     
     func request(sqlstatement: String) {
         
@@ -81,41 +126,36 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             str = str!.stringByReplacingOccurrencesOfString("[Optional({", withString: "[{")
             str = str!.stringByReplacingOccurrencesOfString("})]", withString: "}]")
             
-//                        print("\n\n\n\(str!)\n\n\n");
+//                        print("\n\n\n\(str!)\n\n\n")
             
             let nsdata = str!.dataUsingEncoding(NSUTF8StringEncoding)
             
-            do {
-                let jsonDict: NSArray = try (NSJSONSerialization.JSONObjectWithData(nsdata!, options: NSJSONReadingOptions.MutableContainers) as? NSArray)!
+            var jsonError: NSError?
+            let jsonDict: NSArray = (NSJSONSerialization.JSONObjectWithData(nsdata!, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as? NSArray)!
+            
+            
+            for json in jsonDict {
+                let province = json.valueForKey("province") as! String
+                let amount = json.valueForKey("amount") as! String
                 
-                
-                for json in jsonDict {
-                    let province = json.valueForKey("province") as! String
-                    let amount = json.valueForKey("amount") as! String
-                    
-                    print("ss \(province) \(amount)")
-                    self.pickerDataSource.addObject("\(province)\t\t\(amount)")
-                }
-                
+                print("ss \(province) \(amount)")
+                self.pickerDataSource.addObject("\(province)\t\t\(amount)")
+            }
+            
                 
                 //              print(jsonDict)
-            } catch let error as NSError {
-                print(error)
-            } catch {
-                print("error")
-            }
             
             self.pickerViewProvinces.reloadAllComponents()
             
             
         })
         
-        task!.resume()
+        task.resume()
     }
-    
-    @IBAction func test(sender: AnyObject) {
-        //        request("SELECT * FROM nodeumbo LIMIT 2")
-        request("SELECT s.province AS province, SUM(CASE WHEN smsdown = 'yes' AND smsup = '' THEN 1 ELSE 0 END) AS amount FROM sector s, nodeumbo n WHERE n.node_sector = s.umbo GROUP BY s.province ORDER BY s.province")
-    }
+//    
+//    @IBAction func test(sender: AnyObject) {
+//        //        request("SELECT * FROM nodeumbo LIMIT 2")
+//        request("SELECT s.province AS province, SUM(CASE WHEN smsdown = 'yes' AND smsup = '' THEN 1 ELSE 0 END) AS amount FROM sector s, nodeumbo n WHERE n.node_sector = s.umbo GROUP BY s.province ORDER BY s.province")
+//    }
 }
 
